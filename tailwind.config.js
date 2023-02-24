335234c0afd4325fc794d41afcd5e89b6b6beca1
jaimetrovoada <jaimetrovoada@gmail.com>
2023-02-24 20:01:19 +0800

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      primary: "rgb(var(--color-primary) / 1)",
+      secondary: "rgb(var(--color-secondary) / 1)",
+      tertiary: "rgb(var(--color-tertiary) / 1)",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
