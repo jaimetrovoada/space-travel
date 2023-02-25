4b28bd6e3b0299d6469e3fd0bd7700e759ba44f4
jaimetrovoada <jaimetrovoada@gmail.com>
2023-02-25 18:24:32 +0800

    feat: tablet and desktop homepage

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -3,10 +3,10 @@ import React from "react";
 
 const ExploreBtn = () => {
   return (
-    <button className="h-36 w-36 rounded-full bg-white">
+    <button className="h-36 w-36 rounded-full bg-white md:h-56 md:w-56">
       <Link
         href="/destination"
-        className={`flex h-full w-full items-center justify-center  rounded-full font-bellefair text-xl`}
+        className={`flex h-full w-full items-center justify-center  rounded-full font-bellefair text-xl md:text-3xl`}
       >
         Explore
       </Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,20 +3,24 @@ import ExploreBtn from "@/components/ExploreBtn";
 
 export default function Home() {
   return (
-    <div className="h-full">
-      <p className="text-center text-secondary">SO, YOU WANT TO TRAVEL TO</p>
-      <p
-        className={`my-4 text-center font-bellefair text-[80px] leading-[100px] text-tertiary`}
-      >
-        SPACE
-      </p>
-      <p className="text-center leading-6 text-secondary">
-        Let&apos;s face it; if you want to go to space, you might as well
-        genuinely go to outer space and not hover kind of on the edge of it.
-        Well sit back, and relax because we&apos;ll give you a truly out of this
-        world experience!
-      </p>
-      <div className="mx-auto mt-20 w-fit">
+    <div className="flex h-full flex-col xl:mx-40 xl:flex-row xl:items-end">
+      <div className="flex flex-col items-center text-center xl:mb-32 xl:items-baseline xl:text-left">
+        <p className="text-secondary md:text-xl xl:text-3xl ">
+          SO, YOU WANT TO TRAVEL TO
+        </p>
+        <p
+          className={`my-4 font-bellefair text-[80px] leading-[100px] text-tertiary md:text-[150px] md:leading-[150px]`}
+        >
+          SPACE
+        </p>
+        <p className="leading-6 text-secondary md:max-w-md md:leading-7 xl:text-lg">
+          Let&apos;s face it; if you want to go to space, you might as well
+          genuinely go to outer space and not hover kind of on the edge of it.
+          Well sit back, and relax because we&apos;ll give you a truly out of
+          this world experience!
+        </p>
+      </div>
+      <div className="mx-auto mt-20 w-fit xl:mx-0 xl:mb-32 xl:ml-auto">
         <ExploreBtn />
       </div>
     </div>
