820fe0df735710a0026fcc85ca9974f164874cfc
jaimetrovoada <jaimetrovoada@gmail.com>
2023-02-25 16:29:36 +0800

    add font config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
+import { Barlow_Condensed } from "@next/font/google";
 import type { AppProps } from "next/app";
+
+const barlow = Barlow_Condensed({ weight: "400", subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <style jsx global>{`
+        html {
+          font-family: ${barlow.style.fontFamily};
+        }
+      `}</style>
       <Component {...pageProps} />
     </Layout>
   );
