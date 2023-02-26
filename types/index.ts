158a3756ae5647eb291219695cced2f2727eabbe
jaimetrovoada <jaimetrovoada@gmail.com>
2023-02-26 18:35:36 +0800

    add data types

---
+++ types/index.ts
@@ -0,0 +1,36 @@
+export interface Data {
+  destinations: Destination[];
+  crew: Crew[];
+  technology: Technology[];
+}
+
+export interface Crew {
+  name: string;
+  images: CrewImages;
+  role: string;
+  bio: string;
+}
+
+export interface CrewImages {
+  png: string;
+  webp: string;
+}
+
+export interface Destination {
+  name: string;
+  images: CrewImages;
+  description: string;
+  distance: string;
+  travel: string;
+}
+
+export interface Technology {
+  name: string;
+  images: TechnologyImages;
+  description: string;
+}
+
+export interface TechnologyImages {
+  portrait: string;
+  landscape: string;
+}
