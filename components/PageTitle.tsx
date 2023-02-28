120fa3728abfa102e11a39f6ee68f6fa703571f4
jaimetrovoada <jaimetrovoada@gmail.com>
2023-02-28 21:39:47 +0800

    update pageTitle tablet styling

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -7,7 +7,7 @@ interface Props {
 }
 const PageTitle: React.FC<Props> = ({ sub, title }) => {
   return (
-    <h2 className="text-center font-barlowCondensed tracking-[2.7px] md:self-start md:text-left md:text-xl xl:ml-40 xl:text-[28px] xl:tracking-[4.72px]">
+    <h2 className="text-center font-barlowCondensed tracking-[2.7px] md:self-start md:text-left md:text-xl md:tracking-[3.38px] xl:ml-40 xl:text-[28px] xl:tracking-[4.72px]">
       <strong className="mr-4 text-white/25">{sub}</strong>
       {title}
     </h2>
