d08e17ea713f7a140176dd03247f0eb436fa569e
jaimetrovoada <jaimetrovoada@gmail.com>
2023-02-28 22:06:10 +0800

    add className prop

--- components/Description.tsx
+++ components/Description.tsx
@@ -5,9 +5,14 @@ interface Props {
   className?: string;
 }
 
-const Description: React.FC<Props> = ({ text }) => {
+const Description: React.FC<Props> = ({ text, className }) => {
   return (
-    <p className="text-[15px] leading-6 text-secondary md:text-base md:leading-[28px] xl:text-lg xl:leading-8">
+    <p
+      className={
+        "text-[15px] leading-6 text-secondary md:text-base md:leading-[28px] xl:text-lg xl:leading-8 " +
+        className
+      }
+    >
       {text}
     </p>
   );
