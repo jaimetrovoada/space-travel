30afd14afc7930f9e2c2c876045b35a018affe2d
jaimetrovoada <jaimetrovoada@gmail.com>
2023-03-01 13:22:55 +0800

    add page head titles

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import Nav from "./Nav";
 import { barlowCondensed, bellefair } from "@/styles/fonts";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { routeNamesMap } from "@/utils";
 
 interface Props {
   children: React.ReactNode;
 }
 
 const Layout = ({ children }: Props) => {
+  const router = useRouter();
+  const title = routeNamesMap.get(router.pathname)?.toUpperCase();
   return (
     <>
+      <Head>
+        <title>SPACE TOURISM | {title}</title>
+      </Head>
       <Nav />
       <main
         className={`${bellefair.variable} ${barlowCondensed.variable} flex flex-auto flex-col text-tertiary`}
