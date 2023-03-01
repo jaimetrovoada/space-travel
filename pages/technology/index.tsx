2de23de8ca5a85aacf53d6f85c5b7f1e028e6bbe
jaimetrovoada <jaimetrovoada@gmail.com>
2023-03-01 11:42:06 +0800

    feat: tablet technology page

--- pages/technology/index.tsx
+++ pages/technology/index.tsx
@@ -16,10 +16,10 @@ const Technology = () => {
           }
         `}
       </style>
-      <div>
-        <PageTitle sub="03" title="SPACE LAUNCH 101" />
+      <div className="md:pt-[40px]">
+        <PageTitle sub="03" title="SPACE LAUNCH 101" className="md:ml-9" />
         <div>
-          <div className="my-8">
+          <div className="my-8 md:my-14">
             <Image
               src={tech.images.landscape}
               width={1920}
@@ -32,7 +32,7 @@ const Technology = () => {
               {data.technology.map((data, i) => (
                 <button
                   key={data.name}
-                  className={`h-[40px] w-[40px] rounded-full font-bellefair xl:h-[15px] xl:w-[15px] ${
+                  className={`aspect-square h-[40px] rounded-full font-bellefair md:h-[60px] xl:h-[15px] xl:w-[15px] ${
                     data.name === tech.name
                       ? "bg-white text-black"
                       : "border border-white/25 hover:bg-white/50"
@@ -44,11 +44,14 @@ const Technology = () => {
               ))}
             </div>
             <div className="mt-7">
-              <p className="text-center font-barlowCondensed text-sm tracking-[2.36px] text-secondary">
+              <p className="text-center font-barlowCondensed text-sm tracking-[2.36px] text-secondary md:text-base md:tracking-[2.7px]">
                 THE TERMINOLOGY...
               </p>
-              <Name text={tech.name} className="text-center" />
-              <Description text={tech.description} className="" />
+              <Name text={tech.name} className="text-center md:my-4" />
+              <Description
+                text={tech.description.replaceAll("-", "\u2011")}
+                className=""
+              />
             </div>
           </div>
         </div>
