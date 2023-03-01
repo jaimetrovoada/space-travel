34e87d61251b20493ce9ffd5a69eee237c9cd833
jaimetrovoada <jaimetrovoada@gmail.com>
2023-03-01 11:57:57 +0800

    feat: desktop technology page

--- pages/technology/index.tsx
+++ pages/technology/index.tsx
@@ -17,22 +17,34 @@ const Technology = () => {
         `}
       </style>
       <div className="md:pt-[40px]">
-        <PageTitle sub="03" title="SPACE LAUNCH 101" className="md:ml-9" />
-        <div>
-          <div className="my-8 md:my-14">
+        <PageTitle
+          sub="03"
+          title="SPACE LAUNCH 101"
+          className="md:ml-9 xl:ml-[165px]"
+        />
+        <div className="xl:flex xl:flex-row xl:justify-between">
+          <div className="my-8 md:my-14 xl:order-2">
             <Image
               src={tech.images.landscape}
               width={1920}
               height={1080}
               alt="image of space launch"
+              className="xl:hidden"
+            />
+            <Image
+              src={tech.images.portrait}
+              width={515}
+              height={527}
+              alt="image of space launch"
+              className="hidden xl:block"
             />
           </div>
-          <div className="flex flex-col items-center px-6">
-            <div className="flex flex-row gap-4">
+          <div className="flex flex-col items-center gap-7 px-6 xl:ml-[165px] xl:flex-row xl:gap-20">
+            <div className="flex flex-row gap-4 xl:flex-col xl:gap-8">
               {data.technology.map((data, i) => (
                 <button
                   key={data.name}
-                  className={`aspect-square h-[40px] rounded-full font-bellefair md:h-[60px] xl:h-[15px] xl:w-[15px] ${
+                  className={`aspect-square h-[40px] rounded-full font-bellefair md:h-[60px] xl:h-[80px] xl:text-[32px] ${
                     data.name === tech.name
                       ? "bg-white text-black"
                       : "border border-white/25 hover:bg-white/50"
@@ -43,11 +55,11 @@ const Technology = () => {
                 </button>
               ))}
             </div>
-            <div className="mt-7">
-              <p className="text-center font-barlowCondensed text-sm tracking-[2.36px] text-secondary md:text-base md:tracking-[2.7px]">
+            <div className="text-center xl:text-left">
+              <p className="font-barlowCondensed text-sm tracking-[2.36px] text-secondary md:text-base md:tracking-[2.7px]">
                 THE TERMINOLOGY...
               </p>
-              <Name text={tech.name} className="text-center md:my-4" />
+              <Name text={tech.name} className="md:my-4" />
               <Description
                 text={tech.description.replaceAll("-", "\u2011")}
                 className=""
