44cd4ad15c0bfb58467b91ef53f832cf6b221952
jaimetrovoada <jaimetrovoada@gmail.com>
2023-03-01 10:43:52 +0800

    extend className

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -5,9 +5,14 @@ interface Props {
   title: string;
   className?: string;
 }
-const PageTitle: React.FC<Props> = ({ sub, title }) => {
+const PageTitle: React.FC<Props> = ({ sub, title, className }) => {
   return (
-    <h2 className="text-center font-barlowCondensed tracking-[2.7px] md:self-start md:text-left md:text-xl md:tracking-[3.38px] xl:ml-40 xl:text-[28px] xl:tracking-[4.72px]">
+    <h2
+      className={
+        "text-center font-barlowCondensed tracking-[2.7px] md:self-start md:text-left md:text-xl md:tracking-[3.38px] xl:ml-40 xl:text-[28px] xl:tracking-[4.72px] " +
+        className
+      }
+    >
       <strong className="mr-4 text-white/25">{sub}</strong>
       {title}
     </h2>
