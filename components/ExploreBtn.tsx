c92a4177276a537c22acf6e84211da7e213ba8ff
jaimetrovoada <jaimetrovoada@gmail.com>
2023-03-01 20:18:04 +0800

    fix: homepage mobile styles

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -3,12 +3,12 @@ import React from "react";
 
 const ExploreBtn = () => {
   return (
-    <button className="relative aspect-square h-36 rounded-full bg-white md:h-56 md:w-56 xl:before:transition-all xl:hover:before:absolute xl:hover:before:-inset-1/2 xl:hover:before:-z-10 xl:hover:before:block xl:hover:before:aspect-square xl:hover:before:h-[450px] xl:hover:before:rounded-full  xl:hover:before:bg-white/10">
+    <button className="relative aspect-square h-[150px] rounded-full bg-white md:h-56 md:w-56 xl:before:transition-all xl:hover:before:absolute xl:hover:before:-inset-1/2 xl:hover:before:-z-10 xl:hover:before:block xl:hover:before:aspect-square xl:hover:before:h-[450px] xl:hover:before:rounded-full  xl:hover:before:bg-white/10">
       <Link
         href="/destination"
-        className={`flex h-full w-full items-center  justify-center rounded-full font-bellefair text-xl text-black md:text-3xl`}
+        className={`flex h-full w-full items-center justify-center rounded-full font-bellefair text-[20px] text-xl tracking-[1.25px] text-black md:text-3xl`}
       >
-        Explore
+        EXPLORE
       </Link>
     </button>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import ExploreBtn from "@/components/ExploreBtn";
+import Description from "@/components/Description";
 
 export default function Home() {
   return (
-    <div className="flex h-full flex-col pt-12 md:pt-24 xl:mx-40 xl:flex-row xl:items-end">
+    <div className="flex h-full flex-col px-6 pt-12 md:pt-24 xl:mx-40 xl:flex-row xl:items-end">
       <div className="flex flex-col items-center text-center xl:mb-32 xl:items-baseline xl:text-left">
-        <p className="text-secondary md:text-xl xl:text-3xl ">
+        <p className="tracking-[2.7px] text-secondary md:text-xl xl:text-3xl font-barlowCondensed">
           SO, YOU WANT TO TRAVEL TO
         </p>
         <p
@@ -13,12 +14,14 @@ export default function Home() {
         >
           SPACE
         </p>
-        <p className="leading-6 text-secondary md:max-w-md md:leading-7 xl:text-lg">
-          Let&apos;s face it; if you want to go to space, you might as well
+        <Description
+          text="
+          Let's face it; if you want to go to space, you might as well
           genuinely go to outer space and not hover kind of on the edge of it.
-          Well sit back, and relax because we&apos;ll give you a truly out of
+          Well sit back, and relax because we'll give you a truly out of
           this world experience!
-        </p>
+        "
+        />
       </div>
       <div className="mx-auto mt-20 w-fit xl:mx-0 xl:mb-32 xl:ml-auto">
         <ExploreBtn />
