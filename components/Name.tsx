9b1cbed6c834b29ee51873135d0072e1e1e5e9a3
jaimetrovoada <jaimetrovoada@gmail.com>
2023-03-01 11:28:36 +0800

    create name component

---
+++ components/Name.tsx
@@ -0,0 +1,22 @@
+import React, { HTMLAttributes } from "react";
+
+interface Props {
+  text: string;
+  className?: HTMLAttributes<HTMLElement>["className"];
+}
+
+const Name: React.FC<Props> = ({ text, className }) => {
+  return (
+    <p
+      className={
+        "mt-2 mb-4 font-bellefair text-2xl md:text-[40px] xl:mt-[15px] xl:mb-[27px] xl:text-[56px]" +
+        " " +
+        className
+      }
+    >
+      {text.toUpperCase()}
+    </p>
+  );
+};
+
+export default Name;
