f1ade0a8f5e7fb35ec6a685f97537b98fc10ad1a
jaimetrovoada <jaimetrovoada@gmail.com>
2023-03-02 19:48:41 +0800

    change hover effect to use outline

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const ExploreBtn = () => {
   return (
-    <button className="relative aspect-square h-[150px] rounded-full bg-white md:h-56 md:w-56 xl:before:transition-all xl:hover:before:absolute xl:hover:before:-inset-1/2 xl:hover:before:-z-10 xl:hover:before:block xl:hover:before:aspect-square xl:hover:before:h-[450px] xl:hover:before:rounded-full  xl:hover:before:bg-white/10">
+    <button className="relative aspect-square h-[150px] rounded-full bg-white md:h-56 md:w-56 xl:outline xl:outline-0 xl:outline-white/10 xl:transition-all xl:hover:outline-[88px]">
       <Link
         href="/destination"
         className={`flex h-full w-full items-center justify-center rounded-full font-bellefair text-[20px] text-xl tracking-[1.25px] text-black md:text-3xl`}
