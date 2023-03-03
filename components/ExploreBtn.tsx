f30409f774c503d6f53926795c6b6341d1e1ccdf
jaimetrovoada <jaimetrovoada@gmail.com>
2023-03-03 12:17:07 +0800

    enable hover effects in all screen sizes

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const ExploreBtn = () => {
   return (
-    <button className="relative aspect-square h-[150px] rounded-full bg-white md:h-56 md:w-56 xl:outline xl:outline-0 xl:outline-white/10 xl:transition-all xl:hover:outline-[88px]">
+    <button className="relative aspect-square h-[150px] rounded-full bg-white outline outline-0 outline-white/10 transition-all hover:outline-[88px] md:h-56 md:w-56">
       <Link
         href="/destination"
         className={`flex h-full w-full items-center justify-center rounded-full font-bellefair text-[20px] text-xl tracking-[1.25px] text-black md:text-3xl`}
